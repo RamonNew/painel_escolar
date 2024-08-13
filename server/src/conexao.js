b053e258768871231d3a619deedb1d49d543eb2f
Ramon de Holanda <ramonhnascimento@gmail.com>
2024-08-13 14:12:39 -0300

    criando configuração de env

--- server/src/conexao.js
+++ server/src/conexao.js
@@ -1,8 +1,12 @@
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
 const db = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "painel_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
 }
 
 
