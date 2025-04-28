4923eca480d4dabddd32c89c602fecdba54baf3c
Ramon de Holanda <ramonhnascimento@gmail.com>
2025-04-27 21:14:29 -0300

    refactor(room.model): update to function-based export and standardized naming

--- server/src/controllers/room.controller.js
+++ server/src/controllers/room.controller.js
@@ -1,17 +1,16 @@
-import AmbienteModel from "../models/AmbienteModel.js";
+import { queryRoomsByDateAndPeriod } from "../models/room.model.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import httpStatus from "http-status";
 import ApiError from "../utils/ApiError.js";
 
-// Search for available rooms by date and period
 export const getAvailableRooms = catchAsync(async (req, res) => {
   const { data, periodo } = req.body;
 
   if (!data || !periodo) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Parâmetros data e período são obrigatórios.');
+    throw new ApiError(httpStatus.BAD_REQUEST, "Parâmetros data e período são obrigatórios.");
   }
 
-  const [status, rooms] = await AmbienteModel.listarAmbientesPorDataEPeriodo(data, periodo);
-  
+  const rooms = await queryRoomsByDateAndPeriod(data, periodo);
+
   res.status(httpStatus.OK).json(rooms);
 });
---
+++ server/src/models/room.model.js
@@ -0,0 +1,41 @@
+import mysql from "mysql2/promise";
+import db from "../conexao.js";
+
+const pool = mysql.createPool(db);
+
+export const queryRoomsByDateAndPeriod = async (data, periodo) => {
+  let horaInicio, horaFim;
+
+  switch (periodo) {
+    case 'manha':
+      horaInicio = '00:00:00';
+      horaFim = '12:00:00';
+      break;
+    case 'tarde':
+      horaInicio = '12:00:00';
+      horaFim = '18:00:00';
+      break;
+    case 'noite':
+      horaInicio = '18:00:00';
+      horaFim = '23:59:59';
+      break;
+    default:
+      throw new Error('Período inválido');
+  }
+
+  const dataHoraInicio = `${data} ${horaInicio}`;
+  const dataHoraFim = `${data} ${horaFim}`;
+
+  const sql = `
+    SELECT a.nome AS ambiente, 
+           CASE WHEN aulas.ambiente IS NOT NULL THEN TRUE ELSE FALSE END AS ocupado
+    FROM ambientes a
+    LEFT JOIN aulas ON a.nome = aulas.ambiente 
+    AND aulas.data_hora_inicio BETWEEN ? AND ?
+    ORDER BY ocupado ASC, a.nome ASC
+  `;
+
+  const params = [dataHoraInicio, dataHoraFim];
+  const [retorno] = await pool.query(sql, params);
+  return retorno;
+};
