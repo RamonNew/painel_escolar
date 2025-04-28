a1023a4d0fa1045225774baa25d5074cb85c67f4
Ramon de Holanda <ramonhnascimento@gmail.com>
2025-04-28 18:05:11 -0300

    refactor: remove old imports

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,9 +1,5 @@
-import mysql from "mysql2/promise";
-import db from "../conexao.js";
 import bcrypt from "bcryptjs";
 import pool from "../config/db.js";
-
-//export const pool = mysql.createPool(db);
 
 export const getUserById = async (usuario_id) => {
   const sql = "SELECT * FROM usuarios WHERE usuario_id = ?";
